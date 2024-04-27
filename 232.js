8614b4c9e4ee6f3f967d9266ea6c42e199369d6c
Heo Hao <2483386026@qq.com>
2024-04-27 13:56:22 +0800

    232. 用栈实现队列

---
+++ 232.js
@@ -0,0 +1,50 @@
+var MyQueue = function () {
+    this.arr1 = []
+    this.arr2 = []
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+    this.arr2.push(x)
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+    if (this.arr1.length) {
+        return this.arr1.shift()
+    }
+    if (this.arr2.length) {
+        this.arr1.push(this.arr2.shift())
+        return this.arr1.shift()
+    }
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+    let el = this.pop()
+    this.arr1.push(el)
+    return el
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+    return !this.arr1.length && !this.arr2.length
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
