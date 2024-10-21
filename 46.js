e0477c5e924ebe6359e7901e3eaef9d9a2436100
Heo Hao <2483386026@qq.com>
2024-10-21 16:59:05 +0800

    feat: 46. 全排列

---
+++ 46.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+    const result = [];
+
+    function backtrack(path, used) {
+        if (path.length === nums.length) {
+            result.push([...path]);
+            return;
+        }
+        for (let i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            path.push(nums[i])
+            used[i] = true;
+            backtrack(path, used);
+            path.pop();
+            used[i] = false;
+        }
+    }
+
+    backtrack([], Array(nums.length).fill(false));
+    return result;
+};
