02a08aaa4e503351fe0f53bd43107dba61dcea47
Heo Hao <2483386026@qq.com>
2024-11-01 11:01:23 +0800

    fix: 70. 爬楼梯

--- 70.js
+++ 70.js
@@ -6,7 +6,7 @@ var climbStairs = function (n) {
     if (n <= 2) return n;
     let dp = new Array(n + 1);
     dp[1] = 1;
-    ddp[2] = 2;
+    dp[2] = 2;
     for (let i = 3; i <= n; i++) {
         dp[i] = dp[i - 1] + dp[i - 2];
     }
