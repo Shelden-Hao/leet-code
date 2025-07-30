a4bf9ea3da3bcf6e6920d24d78cd8a8ea6beb1e0
Heo Hao <2483386026@qq.com>
2025-07-30 20:43:53 +0800

    feat: 70. 爬楼梯-动态规划（基本做法）

--- 70.ts
+++ 70.ts
@@ -1,6 +1,7 @@
 /**
- * 爬楼梯
+ * 70. 爬楼梯 - 动态规划（状态压缩）
  * @param n 爬到第 n 阶楼梯
+ * https://leetcode.cn/problems/climbing-stairs/
  */
 function climbStairs(n: number): number {
     let pre = 1
@@ -12,3 +13,20 @@ function climbStairs(n: number): number {
     }
     return cur
 };
+
+/**
+ * 70. 爬楼梯 - 动态规划（基本做法）
+ * @param n
+ */
+// function climbStairs(n: number): number {
+//     // 定义状态
+//     const dp: number[] =[]
+//     // 初始化状态
+//     dp[0] = 1
+//     dp[1] = 1
+//     // 状态转移方程 求出数组的每一项
+//     for (let i = 2; i <= n; i++) {
+//         dp[i] = dp[i - 1] + dp[i - 2]
+//     }
+//     return dp[n]
+// };
