92abd0450f4dc59566de1f250282d6118305c771
Heo Hao <2483386026@qq.com>
2025-08-10 15:13:49 +0800

    feat: quickSort快速排序

---
+++ quickSort.ts
@@ -0,0 +1,48 @@
+/**
+ * 快速排序
+ * @param arr 原数组
+ * @description 使用双指针(left/right)递归拆分原数组，选取某一个元素(我这里选取一个小数组中的最右边元素)为基准元素 pivot。
+ * 然后根据该元素为中间值将数组不断拆分为两个数组，直到递归的左右指针重合，不可再拆分，数组同时也排序完成。
+ */
+function quickSort(arr: number[]): number[] {
+
+    partition(0, arr.length)
+
+    function partition(left: number, right: number) {
+        if (left >= right) return
+
+        // 找到基准元素
+        const pivot = arr[right]
+
+        // 双指针交换 左右都是比基准元素小的 右边都是比基准元素大的
+        let i = left
+        let j = right - 1
+
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+            while (arr[j] > pivot) {
+                j--
+            }
+
+            if (i <= j) {
+                // 此时刚好找到一个左边比基准大的 一个右边比基准小的
+                [arr[i], arr[j]] = [arr[j], arr[i]]
+                // 交换完成还需要给左右指针再移动一次
+                i++;
+                j--
+            }
+        }
+
+        // 此时 i 位置及其左边都是比基准要小的，j 位置及其右边都是比基准要大的 i和j的关系：j<i
+        // 所以需要给pivot放在中间位置，所以是和第一个比他大的元素做交换
+        [arr[i], arr[right]] = [arr[right], arr[i]]
+
+        // 然后继续给左右两边进行partition
+        partition(left, j)
+        partition(i + 1, right)
+    }
+
+    return arr
+}
