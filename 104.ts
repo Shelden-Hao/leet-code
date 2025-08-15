6a690b1155d74b9d85c1af0fce61f5ac8ed5eb0c
Heo Hao <2483386026@qq.com>
2025-08-15 14:41:30 +0800

    feat: 104. 二叉树的最大深度(思路补充)

--- 104.ts
+++ 104.ts
@@ -25,8 +25,10 @@ class TreeNode {
 }
 
 /**
- * 二叉树的最大深度
+ * 104. 二叉树的最大深度
  * @param root 根节点
+ * @description 递归。加一表示加上当前这一层级（一层高度）。
+ * 可以直接思考最后一层（深度为一），具有节点，但是没有left和right，所以max为0，然后再加上一，刚好为该层深度。
  */
 function maxDepth(root: TreeNode | null): number {
     if (root === null) return 0
@@ -37,3 +39,5 @@ function maxDepth(root: TreeNode | null): number {
     // 返回当前层级最大值（深度） 每次加的1都是当前层这一层级
     return Math.max(leftDeep, rightDeep) + 1
 };
+
+export {}
