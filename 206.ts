e15a8320c8dcc87025bd821d8f8827d7c9d2ab08
Heo Hao <2483386026@qq.com>
2025-08-20 10:39:13 +0800

    feat: 206. 反转链表-方法二（循环）-将变量改变成更加语义化的版本

--- 206.ts
+++ 206.ts
@@ -87,41 +87,51 @@ class ListNode {
  * @param head 头节点
  * @description 递归移动节点
  */
+// function reverseList(head: ListNode | null): ListNode | null {
+//     // 判断不必反转的情况边界
+//     if (head === null) return null
+//     if (head.next === null) return head
+//
+//     // 双问号表达式 `??` 使用：当前面表达式的值为 null 或者是 undefined，则返回后面的值，否则返回前面的值。
+//     const newHead = reverseList(head.next)
+//     // 递归到这里开始执行节点指向的操作：
+//     // 最后的迭代为null无以下操作
+//     // 第一次执行以下操作的时候 下面的 head为 倒数第二个节点 上面的 head 是倒数第三个节点
+//     // 上面返回的 newHead 是新的头节点 而下面
+//     head.next.next = head
+//     head.next = null
+//
+//     // console.log(newHead);
+//     // 每次的 newHead 指向的都是 最后一个节点
+//     // 因为传递的 head.next 为空的时候 直接给 head返回 也就是现在的 newHead
+//     return newHead
+// };
+
+/**
+ * 206. 反转链表 - 方法二（循环）- 将变量改变成更加语义化的版本
+ * https://leetcode.cn/problems/reverse-linked-list/description/
+ * @param head 头节点
+ * @description 依次遍历 不断移动节点的指向
+ */
 function reverseList(head: ListNode | null): ListNode | null {
-    // 判断不必反转的情况边界
-    if (head === null) return null
-    if (head.next === null) return head
-
-    // 双问号表达式 `??` 使用：当前面表达式的值为 null 或者是 undefined，则返回后面的值，否则返回前面的值。
-    const newHead = reverseList(head.next)
-    // 递归到这里开始执行节点指向的操作：
-    // 最后的迭代为null无以下操作
-    // 第一次执行以下操作的时候 下面的 head为 倒数第二个节点 上面的 head 是倒数第三个节点
-    // 上面返回的 newHead 是新的头节点 而下面
-    head.next.next = head
-    head.next = null
-
-    // console.log(newHead);
-    // 每次的 newHead 指向的都是 最后一个节点
-    // 因为传递的 head.next 为空的时候 直接给 head返回 也就是现在的 newHead
-    return newHead
+    // 每组两个节点，一个pre，一个cur
+    // 当前“组”第一个节点
+    let pre: ListNode | null = null
+    // 当前“组”第二个（最后一个）节点
+    let cur: ListNode | null = head
+    // 下一组第一个节点
+    let newHead: ListNode | null = null
+    while (cur) {
+        // 临时变量保存下一组第一个节点
+        newHead = cur.next
+        // 当前组第二个节点指向第一个
+        cur.next = pre
+        // 移动 pre 和 cur 到下一组
+        pre = cur
+        cur = newHead
+    }
+    // 最后的时候 cur 为 null，而 pre 刚好为最后一个节点
+    return pre
 };
 
-const node1 = new ListNode(1)
-node1.next = new ListNode(2)
-node1.next.next = new ListNode(3)
-
-const newHead1 = reverseList(node1)
-
-let current = newHead1
-
-const values:number[] = []
-
-while (current) {
-    values.push(current.val)
-    current = current.next
-}
-console.log(values.join('->'));
-
-
-export  {};
+export {};
