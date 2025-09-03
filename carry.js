915a20503de634ed2cb63c6c8f16e9c31367b004
Heo Hao <2483386026@qq.com>
2025-09-03 09:49:57 +0800

    feat: 手写函数柯里化

---
+++ carry.js
@@ -0,0 +1,26 @@
+/**
+ * 函数柯里化 carry
+ * @param fn
+ * @return {(function(...[*]): (*))|*}
+ */
+function curry(fn) {
+    return function curried(...args) {
+        // 如果参数够了，直接执行
+        if (args.length >= fn.length) {
+            return fn.apply(this, args);
+        }
+        // 否则返回一个函数，继续收集参数
+        return function (...nextArgs) {
+            return curried.apply(this, args.concat(nextArgs));
+        };
+    };
+}
+// function sum(a, b, c) {
+//     return a + b + c;
+// }
+//
+// const curriedSum = curry(sum);
+//
+// console.log(curriedSum(1)(2)(3)); // 6
+// console.log(curriedSum(1, 2)(3)); // 6
+// console.log(curriedSum(1)(2, 3)); // 6
