9b45c6068894f866823bce7522c56310dc5e3809
Heo Hao <2483386026@qq.com>
2025-09-10 13:44:28 +0800

    feat: 1. 两数之和 - 变式(数组有序)

--- 1.ts
+++ 1.ts
@@ -1,3 +1,10 @@
+/**
+ * 1. 两数之和
+ * https://leetcode.cn/problems/two-sum/description/
+ * @param nums 原数组
+ * @param target 目标和
+ * @description 空间复杂度和时间复杂度都是 O(n) 。
+ */
 function twoSum(nums: number[], target: number): number[] {
     const map = new Map<number, number>()
     const length = nums.length
@@ -9,5 +16,22 @@ function twoSum(nums: number[], target: number): number[] {
     }
     return [-1, -1]
 };
+
+/**
+ * 1. 两数之和 - 变式(数组有序)
+ * @param nums
+ * @param target
+ * @description 使用双指针，时间复杂度还是 O(n)，但是空间复杂度变为 O(1) 。
+ */
+function twoSumSorted(nums: number[], target: number): number[] {
+    let left = 0, right = nums.length - 1
+    while (left < right) {
+        const sum = nums[left] + nums[right]
+        if (sum === target) return [left, right]
+        if (sum < target) left++
+        else right--
+    }
+    return []
+}
 
 export {}
