bb5ecb09ecf52202f7598f7129e921107922fa41
Heo Hao <2483386026@qq.com>
2025-12-22 22:49:21 +0800

    feat: 手写 Array.prototype.map

--- myMap.js
+++ myMap.js
@@ -3,20 +3,38 @@
  * @param callback 回调函数
  * @param thisArg this指向
  */
+// Array.prototype.myMap = function (callback, thisArg) {
+//     const res = []
+//     return this.reduce((acc, cur, index, arr) => {
+//         // callback 传参：cur 当前正在处理的元素，index 正在处理的元素在数组中的索引，arr 调用了 map 的数组本身
+//         acc.push(callback.call(thisArg, cur, index, arr))
+//         return acc
+//     }, [])
+// };
+
+/**
+ * 手写 Array.prototype.map
+ * @param {Function} callback 回调函数
+ * @param {Object | undefined} thisArg this指向
+ * @returns {Array} 映射后的新数组
+ */
 Array.prototype.myMap = function (callback, thisArg) {
-    const res = []
-    return this.reduce((acc, cur, index, arr) => {
-        // callback 传参：cur 当前正在处理的元素，index 正在处理的元素在数组中的索引，arr 调用了 map 的数组本身
-        acc.push(callback.call(thisArg, cur, index, arr))
-        return acc
-    }, [])
+  const arr = this; // 当前数组
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = callback.call(thisArg, arr[i], i, arr);
+  }
+
+  return result;
 };
 
-const arr = [1, 2, 3, 4];
-
-const mapped = arr.myMap(x => x * 2);
-console.log(mapped); // [2, 4, 6, 8]
-// 支持 thisArg
-const obj = { factor: 3 };
-const mapped2 = arr.myMap(function(x) { return x * this.factor }, obj);
-console.log(mapped2); // [3, 6, 9, 12]
+// const arr = [1, 2, 3, 4];
+// const mapped = arr.myMap((x) => x * 2);
+// console.log(mapped); // [2, 4, 6, 8]
+// // 支持 thisArg
+// const obj = { factor: 3 };
+// const mapped2 = arr.myMap(function (x) {
+//   return x * this.factor;
+// }, obj);
+// console.log(mapped2); // [3, 6, 9, 12]
